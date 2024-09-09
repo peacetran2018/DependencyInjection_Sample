538e03ecbf928c85e45a50e47cd9935f8ce15b78
peacetran2018 <peacetran2018@gmail.com>
2024-09-10 06:38:41 +0800

    first commit

---
+++ Services/CitiesService.cs
@@ -0,0 +1,19 @@
+﻿namespace Serivces;
+public class CitiesService
+{
+    private List<string> _cities;
+
+    public CitiesService(){
+        _cities = new List<string>(){
+            "Vietnam",
+            "Singapore",
+            "Australia",
+            "Japan",
+            "USA"
+        };
+    }
+
+    public List<string> GetCities(){
+        return _cities;
+    }
+}
